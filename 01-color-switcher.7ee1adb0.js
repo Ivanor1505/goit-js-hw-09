0c6ae6fe1236619dbc2719a781e3ab7a8bc54f94
Ivanor1505 <124795324+Ivanor1505@users.noreply.github.com>
2023-07-03 10:31:57 +0000

    Deploying to gh-pages from @ Ivanor1505/goit-js-hw-09@5e38c60a84bc45c7e7c7668c07100002d96baf1f 🚀

---
+++ 01-color-switcher.7ee1adb0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),console.log(n)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.7ee1adb0.js.map
