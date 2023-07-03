96bccfdd7dc11d573b0e547752ef4fd79dea4d82
Ivanor1505 <ivanor1505@gmail.com>
2023-07-03 13:29:40 +0300

    hw 9.1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const start = document.querySelector("[data-start]");
+const stop = document.querySelector("[data-stop]");
+let intervalId = null;
+
+start.addEventListener("click", onStart);
+stop.addEventListener("click", onStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onStart() {
+    start.disabled = true;
+    intervalId = setInterval(() => { document.body.style.backgroundColor = getRandomHexColor(); }, 1000);
+    console.log(intervalId);
+ };
+
+
+
+function onStop() {
+    clearInterval(intervalId);
+  start.disabled = false;
+ };
